d2add60ed4703c4f621f872db5f5ba4d36e3fd30
analiarojas2900 <analia.rojas@duocuc.cl>
2023-05-06 21:57:10 -0400

    commits

--- cesfam/core/urls.py
+++ cesfam/core/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',views.registro,name='registro'),
+    path('',views.inventario,name='inventario'),
     path('login',views.login,name='login'),
     path('menu',views.menu,name='menu'),
-    path('inventario',views.inventario,name='inventario'),
     path('buscar',views.buscar,name='buscar'),
     path('medicamentos',views.medicamentos,name='medicamentos'),
 ]
