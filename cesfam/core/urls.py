137407310c1f818320e4cee559b9a7db14d3c540
analiarojas2900 <analia.rojas@duocuc.cl>
2023-05-19 09:59:32 -0400

    commits

--- cesfam/core/urls.py
+++ cesfam/core/urls.py
@@ -5,14 +5,25 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
+
+    #registro
     path('',views.registro,name='registro'),
-    path('inventario',views.inventario,name='inventario'),
     path('login',views.login,name='login'),
-    path('menu',views.menu,name='menu'),
-    path('buscar',views.buscar,name='buscar'),
+    #farmaceutico
     path('medicamentos',views.medicamentos,name='medicamentos'),
+    path('registromedicamento',views.registromedicamento,name='registromedicamento'),
+    path('retiromedicamento',views.retiromedicamento,name='retiromedicamento'),
+    path('stock',views.stock,name='stock'),
+    path('menufarmaceutico',views.menufarmaceutico,name='menufarmaceutico'),
+#medico
     path('ajustes',views.ajustes,name='ajustes'),
+    path('buscar',views.buscar,name='buscar'),
+    path('menu',views.menu,name='menu'),
     path('perfil',views.perfil,name='perfil'),
     path('prescripcion',views.prescripcion,name='prescripcion'),
+    path('inventario',views.inventario,name='inventario'),
     path('reserva',views.reserva,name='reserva'),
+#paciente
+    path('tomahora',views.tomahora,name='toma de hora'),
 ]
+
--- cesfam/core/views.py
+++ cesfam/core/views.py
@@ -2,33 +2,71 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def registro(request):
-    return render(request,'core/registro.html')
-
-
+#registro
 def login(request):
-    return render(request,'core/login.html')
+    return render(request,'core/registro/login.html')
 
-def menu(request):
-    return render(request,'core/menu.html')
+def registro(request):
+    return render(request,'core/registro/registro.html')
 
-def inventario(request):
-    return render(request,'core/inventario.html')
-    
-def buscar(request):
-    return render(request,'core/buscar.html')
+
+
+#//fin de registro
+
+
+#farmaceutico
+
+
 
 def medicamentos(request):
-    return render(request,'core/medicamentos.html')
+    return render(request,'core/farmaceutico/medicamentos.html')
 
-def perfil(request):
-    return render(request,'core/perfil.html')
+def registromedicamento(request):
+    return render(request,'core/farmaceutico/registromedicamento.html')   
 
-def prescripcion(request):
-    return render(request,'core/prescripcion.html')
+def retiromedicamento(request):
+    return render(request,'core/farmaceutico/retiromedicamento.html') 
 
-def reserva(request):
-    return render(request,'core/reserva.html')
+def stock(request):
+    return render(request,'core/farmaceutico/stock.html')
+
+def menufarmaceutico(request):
+    return render(request,'core/farmaceutico/menufarmaceutico.html')
+
+#fin de farmaceutico
+
+
+#medic
 
 def ajustes(request):
-    return render(request,'core/ajustes.html')
+    return render(request,'core/medico/ajustes.html')
+
+def buscar(request):
+    return render(request,'core/medico/buscar.html')
+
+def inventario(request):
+    return render(request,'core/medico/inventario.html')   
+
+def menu(request):
+    return render(request,'core/medico/menu.html')
+
+def perfil(request):
+    return render(request,'core/medico/perfil.html')
+
+def prescripcion(request):
+    return render(request,'core/medico/prescripcion.html')
+
+def reserva(request):
+    return render(request,'core/medico/reserva.html')
+
+
+
+#fin de medico
+
+
+#paciente
+
+def tomahora(request):
+    return render(request,'core/paciente/tomahora.html')
+
+#fin paciente
